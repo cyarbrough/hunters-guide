3cbd372b7b4b7c04cc93b105444a0231bfef162a
C. Yarbrough <cryarbrough@gmail.com>
2018-02-19 09:02:05 -0700

    Added font-awesome v5; removed close/search icons

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,5 +20,8 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import('vendor/fa/fontawesome.min.js');
+  app.import('vendor/fa/fa-solid.min.js');
+
   return app.toTree();
 };
