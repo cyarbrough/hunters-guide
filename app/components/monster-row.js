63cc20e4f59aaa1770cf06cc8a6de1a67bddaaf3
C. Yarbrough <cryarbrough@gmail.com>
2018-02-22 22:05:23 -0700

    Added inViewport and settings for monster images

--- app/components/monster-row.js
+++ app/components/monster-row.js
@@ -1,10 +1,33 @@
 import Component from '@ember/component';
+import Ember from 'ember';
+import InViewportMixin from 'ember-in-viewport';
 
-export default Component.extend({
+export default Component.extend(InViewportMixin, {
   /**
    * Overrides
    */
   classNames: ['monster-row'],
+  classNameBindings: [ 'viewportEntered:is-active' ],
   
-  monster: null
+  inViewport: false,
+  monster: null,
+
+  viewportOptionsOverride: Ember.on('didInsertElement', function() {
+    Ember.setProperties(this, {
+      viewportEnabled: true,
+      viewportUseRAF: true,
+      viewportSpy: false,
+      viewportUseIntersectionObserver: true,
+      viewportScrollSensitivity: 1,
+      viewportRefreshRate: 150,
+      intersectionThreshold: 0,
+      scrollableArea: null,
+      viewportTolerance: {
+        top: 250,
+        bottom: 250,
+        left: 20,
+        right: 20
+      }
+    });
+  })
 });
