6885e8ae57a3c2b9d39b37fa1fc7185592234739
C. Yarbrough <cryarbrough@gmail.com>
2023-11-15 23:58:44 -0700

    Package updates to 4.0

--- config/targets.js
+++ config/targets.js
@@ -1,8 +1,7 @@
-module.exports = {
-  browsers: [
-    'ie 9',
-    'last 1 Chrome versions',
-    'last 1 Firefox versions',
-    'last 1 Safari versions'
-  ]
-};
+'use strict';
+
+const browsers = [
+  'last 1 Chrome versions',
+  'last 1 Firefox versions',
+  'last 1 Safari versions'
+];
