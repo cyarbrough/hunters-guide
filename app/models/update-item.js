a9b38665ae05d4a03570aba9eafda32546a7500b
C. Yarbrough <cryarbrough@gmail.com>
2024-01-07 22:34:44 -0700

    Update update-item component

--- app/models/update-item.js
+++ app/models/update-item.js
@@ -3,4 +3,13 @@ import Model, { attr } from '@ember-data/model';
 export default class UpdateItemModel extends Model {
   @attr('string') date;
   @attr('string') title;
+
+  get formattedDate() {
+    const date = new Date(this.date);
+    const year = String(date.getFullYear());
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+
+    return `${year}.${month}.${day}`;
+  }
 }
