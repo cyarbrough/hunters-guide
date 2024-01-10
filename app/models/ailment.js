385b3025ca354fefd3c77c092f07b6187adc201b
C. Yarbrough <cryarbrough@gmail.com>
2024-01-09 23:21:21 -0700

    Update ailment-info component

--- app/models/ailment.js
+++ app/models/ailment.js
@@ -3,4 +3,9 @@ import Model, { attr } from '@ember-data/model';
 export default class AilmentModel extends Model {
   @attr('string') name;
   @attr('string') slug;
+
+  get cssName() {
+    let ailmentName = this.slug || 'unknown';
+    return `is-ailment-${ailmentName}`;
+  }
 }
